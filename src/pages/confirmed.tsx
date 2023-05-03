b256f5ecf3fb767e44b3b5100f666d89c2c77ca8
mar-alv <marcelodossantosalvarez@gmail.com>
2023-05-03 20:34:10 -0300

    refactor: Gadient border on last page

--- src/pages/confirmed.tsx
+++ src/pages/confirmed.tsx
@@ -13,48 +13,50 @@ export function Confirmed() {
         </h2>
       </div>
       <div className='flex mt-10 justify-between'>
-        <div className='p-10 border-solid border-yellow border-2 gap-10 flex flex-col rounded-tl-md rounded-br-md rounded-tr-[2.25rem] rounded-bl-[2.25rem]'>
-          <div className='flex items-center'>
-            <div className='bg-purple mr-3 p-2 rounded-full'>
-              <MapPin size={16} weight='fill' className='text-background' />
+        <div className='flex items-center bg-gradient-to-r from-yellow via-purpleLight to-purple rounded-tl-md rounded-br-md rounded-tr-[2.25rem] rounded-bl-[2.25rem]'>
+          <div className='flex flex-col px-1'>
+            <div className='flex items-center bg-background px-9 py-6 rounded-tl-md rounded-tr-[2.25rem]'>
+              <div className='bg-purple mr-3 p-2 rounded-full'>
+                <MapPin size={16} weight='fill' className='text-background' />
+              </div>
+              <div className='text-baseText font-roboto'>
+                <p>
+                  Entrega em <b>Rua João Daniel Martinelli, 102</b>
+                </p>
+                <p>
+                  Farrapos - Porto Alegre, RS
+                </p>
+              </div>
             </div>
-            <div className='text-baseText font-roboto'>
-              <p>
-                Entrega em <b>Rua João Daniel Martinelli, 102</b>
-              </p>
-              <p>
-                Farrapos - Porto Alegre, RS
-              </p>
+            <div className='flex items-center bg-background px-9 py-6'>
+              <div className='bg-yellow mr-3 p-2 rounded-full'>
+                <Timer size={16} weight='fill' className='text-background' />
+              </div>
+              <div className='text-baseText font-roboto'>
+                <p>
+                  Previsão de entrega
+                </p>
+                <p>
+                  <b>
+                    20 min - 30 min
+                  </b>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center'>
-            <div className='bg-yellow mr-3 p-2 rounded-full'>
-              <Timer size={16} weight='fill' className='text-background' />
-            </div>
-            <div className='text-baseText font-roboto'>
-              <p>
-                Previsão de entrega
-              </p>
-              <p>
-                <b>
-                  20 min - 30 min
-                </b>
-              </p>
-            </div>
-          </div>
-          <div className='flex items-center'>
-            <div className='bg-yellowDark mr-3 p-2 rounded-full'>
-              <CurrencyDollar size={16} weight='fill' className='text-background' />
-            </div>
-            <div className='text-baseText font-roboto'>
-              <p>
-                Pagamento na entrega
-              </p>
-              <p>
-                <b>
-                  Cartão de Crédito
-                </b>
-              </p>
+            <div className='flex items-center bg-background px-9 py-6 rounded-br-md rounded-bl-[2.25rem]'>
+              <div className='bg-yellowDark mr-3 p-2 rounded-full'>
+                <CurrencyDollar size={16} weight='fill' className='text-background' />
+              </div>
+              <div className='text-baseText font-roboto'>
+                <p>
+                  Pagamento na entrega
+                </p>
+                <p>
+                  <b>
+                    Cartão de Crédito
+                  </b>
+                </p>
+              </div>
             </div>
           </div>
         </div>
