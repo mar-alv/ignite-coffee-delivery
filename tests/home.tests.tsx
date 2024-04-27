ac16f2831ec1bbba21d2afff3d9484529595be7b
mar-alv <mar.alv.cod@gmail.com>
2024-04-27 16:47:15 -0300

    Issue #13 - coffee amount buttons tests

--- tests/home.tests.tsx
+++ tests/home.tests.tsx
@@ -1,21 +1,10 @@
 import '@testing-library/jest-dom'
 import { CoffeeList } from '@components'
-import { customRender, customRerender } from './test-utils'
-import { fireEvent, screen } from '@testing-library/react'
+import { coffees } from '../src/coffees'
+import { customRender } from './test-utils'
+import { screen } from '@testing-library/react'
 
-describe('Home Tests', () => {
-  const taskDone = {
-    id: '0',
-    content: 'Some task to be done',
-    isDone: true
-  }
-
-  const taskNotDone = {
-    id: '0',
-    content: 'Some task to be done',
-    isDone: false
-  }
-
+describe('home Tests', () => {
   it('should render all coffees', () => {
     // arrange
     customRender(<CoffeeList />)
@@ -24,99 +13,6 @@ describe('Home Tests', () => {
     const coffeesCount = screen.getAllByText('R$').length
 
     // assert
-    expect(coffeesCount).toBe(14)
+    expect(coffeesCount).toBe(coffees.length)
   })
-
-  /* it('should not show the check icon if the task is not done', () => {
-    // arrange
-    customRender(<Checkbox task={taskNotDone} />, { tasks: [taskNotDone] })
-
-    // act
-    const checkIcon = screen.queryByRole('check-icon')
-
-    // assert
-    expect(checkIcon).toBeNull()
-  })
-
-  it('should toggle the task as done when clicking on its label', () => {
-    // arrange
-    const { rerender } = customRender(<Checkbox task={taskNotDone} />, { tasks: [taskNotDone] })
-
-    // act
-    const checkbox = screen.getByText('Some task to be done')
-
-    fireEvent.click(checkbox)
-
-    customRerender(rerender, <Checkbox task={taskDone} />, {
-      tasks: [taskDone]
-    })
-
-    const checkIcon = screen.queryByRole('check-icon')
-  
-    // assert
-    expect(checkIcon).toBeInTheDocument()
-  })
-
-  it('should toggle the task as not done when clicking on its label', () => {
-    // arrange
-    const { rerender } = customRender(<Checkbox task={taskDone} />, { tasks: [taskDone] })
-
-    // act
-    const checkbox = screen.getByText('Some task to be done')
-
-    fireEvent.click(checkbox)
-
-    customRerender(rerender, <Checkbox task={taskNotDone} />, {
-      tasks: [taskNotDone]
-    })
-
-    const checkIcon = screen.queryByRole('check-icon')
-
-    // assert
-    expect(checkIcon).toBeNull()
-  })
-
-  it('should toggle the task as done when clicking on enter by having its checkbox focused', () => {
-    // arrange
-    const { rerender } = customRender(<Checkbox task={taskNotDone} />, { tasks: [taskNotDone] })
-
-    // act
-    const checkbox = screen.getByTestId('checkbox0')
-
-    fireEvent.keyDown(checkbox, {
-      key: 'Enter',
-      keyCode: 13
-    })
-
-    customRerender(rerender, <Checkbox task={taskDone} />, {
-      tasks: [taskDone]
-    })
-
-    const checkIcon = screen.queryByRole('check-icon')
-
-    // assert
-    expect(checkIcon).toBeInTheDocument()
-  })
-
-  it('should toggle the task as not done when clicking on enter by having its checkbox focused', () => {
-    // arrange
-    const { rerender } = customRender(<Checkbox task={taskDone} />, { tasks: [taskDone] })
-
-    // act
-    const checkbox = screen.getByTestId('checkbox0')
-
-    fireEvent.keyDown(checkbox, {
-      key: 'Enter',
-      keyCode: 13
-    })
-
-    customRerender(rerender, <Checkbox task={taskNotDone} />, {
-      tasks: [taskNotDone]
-    })
-
-    const checkIcon = screen.queryByRole('check-icon')
-
-    // assert
-    expect(checkIcon).toBeNull()
-  }) */
 })
